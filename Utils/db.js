001c1d488046d7fa34b44c2064ebcea5b54c1768
CynthiaOguamanam <cynthiadymphna04@gmail.com>
2022-06-11 05:33:54 +0100

    files

--- Utils/db.js
+++ Utils/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const uri = "mongodb://localhost/bootDb";
+// const uri = "mongodb://localhost/bootDb";
+const uri = "mongodb+srv://richydymphna_04:cynthia04@cluster0.vgqm4.mongodb.net/Bootcamp?retryWrites=true&w=majority";
 
 mongoose.connect(uri).then(() => {
 	console.log("database is now connected...!");
